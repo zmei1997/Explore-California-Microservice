f3cd159ba010d3e9e646916209adf1072a9a61cf
zmei1997 <68665894+zmei1997@users.noreply.github.com>
2021-10-25 21:38:50 -0400

    working on ExplorecaliApplication class

--- explorecali/src/main/java/com/example/ec/ExplorecaliApplication.java
+++ explorecali/src/main/java/com/example/ec/ExplorecaliApplication.java
@@ -1,13 +1,116 @@
 package com.example.ec;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ExplorecaliApplication {
+import com.example.ec.domain.Difficulty;
+import com.example.ec.domain.Region;
+import com.example.ec.service.TourPackageService;
+import com.example.ec.service.TourService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
 
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
+import static com.fasterxml.jackson.annotation.PropertyAccessor.FIELD;
+
+@SpringBootApplication
+public class ExplorecaliApplication implements CommandLineRunner {
+	@Value("${ec.importfile}")
+    private String importFile;
+	
+	@Autowired
+    private TourPackageService tourPackageService;
+
+    @Autowired
+    private TourService tourService;
+    
 	public static void main(String[] args) {
 		SpringApplication.run(ExplorecaliApplication.class, args);
 	}
+	
+	@Override
+    public void run(String... args) throws Exception {
+        createTourPackages();
+//        long numOfTourPackages = tourPackageService.total();
+        createTours(importFile);
+//        long numOfTours = tourService.total();
+    }
+	
+//	@Override
+//	private void run(String... args) throws IOException {
+//        //Create the Tour Packages
+//        createTourPackages();
+//        long numOfPackages = tourPackageService.total();
+//
+//        //Load the tours from an external Json File
+//        createTours("ExploreCalifornia.json");
+//        long numOfTours = tourService.total();
+//    }
+
+	private void createTours(String fileToImport) throws IOException {
+		TourFromFile.read(fileToImport).forEach(importedTour ->
+        tourService.createTour(importedTour.getTitle(),
+                importedTour.getDescription(),
+                importedTour.getBlurb(),
+                importedTour.getPrice(),
+                importedTour.getLength(),
+                importedTour.getBullets(),
+                importedTour.getKeywords(),
+                importedTour.getPackageType(),
+                importedTour.getDifficulty(),
+                importedTour.getRegion()));
+		
+	}
+
+	private void createTourPackages(){
+        tourPackageService.createTourPackage("BC", "Backpack Cal");
+        tourPackageService.createTourPackage("CC", "California Calm");
+        tourPackageService.createTourPackage("CH", "California Hot springs");
+        tourPackageService.createTourPackage("CY", "Cycle California");
+        tourPackageService.createTourPackage("DS", "From Desert to Sea");
+        tourPackageService.createTourPackage("KC", "Kids California");
+        tourPackageService.createTourPackage("NW", "Nature Watch");
+        tourPackageService.createTourPackage("SC", "Snowboard Cali");
+        tourPackageService.createTourPackage("TC", "Taste of California");
+    }
+	
+	private static class TourFromFile {
+        //fields
+        private String packageType, title, description, blurb, price, length,
+                bullets, keywords, difficulty, region;
+        //reader
+        static List<TourFromFile> read(String fileToImport) throws IOException {
+            return new ObjectMapper().setVisibility(FIELD, ANY).
+                    readValue(new FileInputStream(fileToImport), new TypeReference<List<TourFromFile>>() {});
+        }
+        protected TourFromFile(){}
+
+        String getPackageType() { return packageType; }
+
+        String getTitle() { return title; }
+
+        String getDescription() { return description; }
+
+        String getBlurb() { return blurb; }
+
+        Integer getPrice() { return Integer.parseInt(price); }
+
+        String getLength() { return length; }
+
+        String getBullets() { return bullets; }
+
+        String getKeywords() { return keywords; }
+
+        Difficulty getDifficulty() { return Difficulty.valueOf(difficulty); }
+
+        Region getRegion() { return Region.findByLabel(region); }
+    }
 
 }
--- explorecali/src/main/java/com/example/ec/repo/TourPackageRepository.java
+++ explorecali/src/main/java/com/example/ec/repo/TourPackageRepository.java
@@ -1,9 +1,12 @@
 package com.example.ec.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.ec.domain.TourPackage;
 
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
+	Optional<TourPackage> findByName(String name);
 
 }

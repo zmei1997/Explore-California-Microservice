58924da728e99f31bd9001399e8460b727ade81e
zmei1997 <68665894+zmei1997@users.noreply.github.com>
2021-10-27 22:41:22 -0400

    add, get, delete functions

--- explorecali/src/main/java/com/example/ec/repo/TourPackageRepository.java
+++ explorecali/src/main/java/com/example/ec/repo/TourPackageRepository.java
@@ -8,5 +8,27 @@ import com.example.ec.domain.TourPackage;
 
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
 	Optional<TourPackage> findByName(String name);
+	@Override
+    @RestResource(exported = false)
+    <S extends TourPackage> S save(S s);
 
+    @Override
+    @RestResource(exported = false)
+    <S extends TourPackage> Iterable<S> saveAll(Iterable<S> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(String s);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(TourPackage tourPackage);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends TourPackage> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
--- explorecali/src/main/java/com/example/ec/repo/TourRepository.java
+++ explorecali/src/main/java/com/example/ec/repo/TourRepository.java
@@ -7,6 +7,31 @@ import org.springframework.data.repository.query.Param;
 
 import com.example.ec.domain.Tour;
 
-public interface TourRepository extends CrudRepository<Tour, Integer> {
-	List<Tour> findByTourPackageCode(@Param("code")String code);
+public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
+
+    Page<Tour> findByTourPackageCode(@Param("code")String code, Pageable pageable);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> S save(S s);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> Iterable<S> saveAll(Iterable<S> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Tour tour);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Tour> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }

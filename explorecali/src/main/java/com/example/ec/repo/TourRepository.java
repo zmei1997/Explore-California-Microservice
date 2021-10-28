93a3fff7d613e865727722a9cacef1c2542cb3b6
zmei1997 <68665894+zmei1997@users.noreply.github.com>
2021-10-27 23:07:14 -0400

    fix Package issues

--- explorecali/src/main/java/com/example/ec/repo/TourRepository.java
+++ explorecali/src/main/java/com/example/ec/repo/TourRepository.java
@@ -1,9 +1,10 @@
 package com.example.ec.repo;
 
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.example.ec.domain.Tour;
 
